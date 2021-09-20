dfdab59cb009937d09e01b5c5900f6d5435ef7ac
Alex Pop <alexandru-stefan.pop@audi.de>
2021-09-20 15:09:11 +0200

    update to user story step 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String[][] gridArray = new String[5][5];
+        String ships = "X";
 
         for(int i = 0; i < gridArray.length; i++){
             for(int y = 0; y < gridArray.length; y++){
@@ -12,11 +13,45 @@ public class Main {
             }
         }
 
-        System.out.println("| " + gridArray[0][0] + " - " + gridArray[0][1] + " - " + gridArray[0][2] + " - "+ gridArray[0][3] + " - "+ gridArray[0][4] + " |\n" +
-                "| " + gridArray[1][0] + " - " + gridArray[1][1] + " - " + gridArray[1][2] + " - "+ gridArray[1][3] + " - "+ gridArray[1][4] + " |\n" +
-                "| " + gridArray[2][0] + " - " + gridArray[2][1] + " - " + gridArray[2][2] + " - "+ gridArray[2][3] + " - "+ gridArray[2][4] + " |\n" +
-                "| " + gridArray[3][0] + " - " + gridArray[3][1] + " - " + gridArray[3][2] + " - "+ gridArray[3][3] + " - "+ gridArray[3][4] + " |\n" +
-                "| " + gridArray[4][0] + " - " + gridArray[4][1] + " - " + gridArray[4][2] + " - "+ gridArray[4][3] + " - "+ gridArray[4][4] + " |\n");
+        System.out.println("| " + gridArray[0][0] + " - " + gridArray[0][1] + " - " + gridArray[0][2] +
+                " - "+ gridArray[0][3] + " - "+ gridArray[0][4] + " |\n" +
+
+                "| " + gridArray[1][0] + " - " + gridArray[1][1] + " - " + gridArray[1][2] +
+                " - "+ gridArray[1][3] + " - "+ gridArray[1][4] + " |\n" +
+
+                "| " + gridArray[2][0] + " - " + gridArray[2][1] + " - " + gridArray[2][2] +
+                " - "+ gridArray[2][3] + " - "+ gridArray[2][4] + " |\n" +
+
+                "| " + gridArray[3][0] + " - " + gridArray[3][1] + " - " + gridArray[3][2] +
+                " - "+ gridArray[3][3] + " - "+ gridArray[3][4] + " |\n" +
+
+                "| " + gridArray[4][0] + " - " + gridArray[4][1] + " - " + gridArray[4][2] +
+                " - "+ gridArray[4][3] + " - "+ gridArray[4][4] + " |\n");
+
+
+        System.out.println("Please choose your spot");
+
+        int eingabe1 = scanner.nextInt();
+        int eingabe2 = scanner.nextInt();
+
+
+        gridArray[eingabe2][eingabe1] = ships;
+
+
+        System.out.println("| " + gridArray[0][0] + " - " + gridArray[0][1] + " - " + gridArray[0][2] +
+                " - "+ gridArray[0][3] + " - "+ gridArray[0][4] + " |\n" +
+
+                "| " + gridArray[1][0] + " - " + gridArray[1][1] + " - " + gridArray[1][2] +
+                " - "+ gridArray[1][3] + " - "+ gridArray[1][4] + " |\n" +
+
+                "| " + gridArray[2][0] + " - " + gridArray[2][1] + " - " + gridArray[2][2] +
+                " - "+ gridArray[2][3] + " - "+ gridArray[2][4] + " |\n" +
+
+                "| " + gridArray[3][0] + " - " + gridArray[3][1] + " - " + gridArray[3][2] +
+                " - "+ gridArray[3][3] + " - "+ gridArray[3][4] + " |\n" +
+
+                "| " + gridArray[4][0] + " - " + gridArray[4][1] + " - " + gridArray[4][2] +
+                " - "+ gridArray[4][3] + " - "+ gridArray[4][4] + " |\n");
 
     }
 
